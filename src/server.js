9511ea9dbdd1913e777b6b26de3ebd4dc195537e
yurii <robka4an@gmail.com>
2024-06-11 10:09:49 +0300

    finish mongo

--- src/server.js
+++ src/server.js
@@ -4,10 +4,14 @@ import cors from 'cors';
 
 import { env } from './utils/env.js';
 
+import dotenv from 'dotenv';
+import { getAllStudents, getStudentById } from './services/students.js';
+
 const PORT = Number(env('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
+  dotenv.config();
 
   app.use(express.json());
   app.use(cors());
@@ -20,22 +24,42 @@ export const startServer = () => {
     }),
   );
 
-  app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello world!',
+  app.get('/students', async (req, res) => {
+    const students = await getAllStudents();
+
+    res.status(200).json({
+      data: students,
     });
   });
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
+  app.get('/students/:studentId', async (req, res, next) => {
+    const { studentId } = req.params;
+    const student = await getStudentById(studentId);
+
+    // Відповідь, якщо контакт не знайдено
+    if (!student) {
+      res.status(404).json({
+        message: 'Student not found',
+      });
+      return;
+    }
+
+    // Відповідь, якщо контакт знайдено
+    res.status(200).json({
+      data: student,
     });
   });
 
-  app.use((err, req, res, next) => {
+  app.use((err, req, res) => {
     res.status(500).json({
       message: 'Something went wrong',
       error: err.message,
+    });
+  });
+
+  app.use('*', (req, res) => {
+    res.status(404).json({
+      message: 'Not found',
     });
   });
 
@@ -43,9 +67,5 @@ export const startServer = () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
-
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // Читаємо змінну оточення PORT
